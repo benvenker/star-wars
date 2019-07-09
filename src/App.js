74728179d50478c54ac6d4a0ab36646009692ac8
Ben Venker <bvemails@gmail.com>
2019-07-09 00:52:49 -0700

    move app component to index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Card from './Card';
+import Header from './Header';
+import CardList from './CardList';
+import { people } from './people';
 
-function App() {
-  return (
-    <Card />
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      people: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch(`https://swapi.co/api/people/`)
+      .then(response => response.json())
+      .then(people => this.setState({ people: people }));
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />,
+      <CardList people={people} />
+      </div>
+    );
+  }
 }
+
+
+// function App() {
+//   return (
+//     <div>
+//       <Header />,
+//       <CardList people={people} />
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div><h1 className="f1 tc">Star Wars Data API</h1></div>
+  )
+}
+
+export default Header;
